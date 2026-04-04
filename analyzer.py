66288a4aecd6a5cdf4e3bdcefd51c355bf908065
HamitMizrak <hamitmizrak@gmail.com>
2026-04-04 14:58:41 +0300

    analyzer.py import, from

---
+++ analyzer.py
@@ -0,0 +1,23 @@
+"""
+Bu dosya: projenin veri analizi ve rapor üretim mantığını taşır.
+Bizim amacımız: kodu parçalara bölmek ve her bir parçayı SOLID(Single Responsibility) göre yönetmek  ==> Yani Modüler olmasını sağlamak
+"""
+
+# 1-) Gerekli kütüphaneleri içe aktarıyoruz.
+"""
+    Pandas     => Veri işleme
+    Numpy      => Sayısal Hesaplama
+    Matplotlib => Grafik üretimi
+    Beartype   => Fonksiyonlara tip kontrolü için
+"""
+
+from __future__ import annotations
+from pathlib import Path
+from typing import Any
+import base64
+import io
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
